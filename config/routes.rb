fbe42910ecae6d68b1bc1c36162f6dcec03a8fd3
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-04-16 13:58:19 +0100

    Add basic status pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'main#show'
   get 'main', to: 'main#show', format: :json
+  get 'ping', to: 'status#ping', format: :json
+  get 'healthcheck', to: 'status#status', format: :json
+  get 'status', to: 'status#ping', format: :json
 end
