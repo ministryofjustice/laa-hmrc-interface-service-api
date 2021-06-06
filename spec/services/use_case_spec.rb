01c78332a32d2ff9fe0aa5eb3189366b6c1ccdac
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-06-06 21:30:34 +0100

    Update UseCase test
    Stub redis call and ensure bearer_token is not invoked

--- spec/services/use_case_spec.rb
+++ spec/services/use_case_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe UseCase do
   subject(:use_case) { described_class.new }
 
-  it { is_expected.to be_a UseCase }
+  describe 'initializing' do
+    before { use_case }
 
-  context 'when a valid bearer_token is available' do
-    before { REDIS.set('use_case_1_bearer_token', 'fake_token_value') }
+    context 'when a valid bearer_token is available' do
+      before { REDIS.set('use_case_1_bearer_token', 'fake_token_value') }
 
-    it 'does not call the bearer_token generator' do
-      expect(BearerToken).not_to receive(:call)
+      it 'does not call the bearer_token generator' do
+        expect(BearerToken).not_to receive(:call)
+      end
     end
   end
 end
