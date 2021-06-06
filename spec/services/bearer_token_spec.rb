c8930eec2a691a7ca4d9c96533a9910a751bad23
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2021-06-06 18:41:23 +0100

    Update bearer_token tests - make rubocop happy

--- spec/services/bearer_token_spec.rb
+++ spec/services/bearer_token_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe BearerToken do
   subject(:bearer_token) { described_class.new }
 
-  let(:faked_response) do
+  let(:fake_data) do
     {
       access_token: 'zz00000z00z0z00000z0z0z0000z0000',
       token_type: 'bearer'
@@ -12,7 +12,7 @@ describe BearerToken do
   end
 
   before do
-    stub_request(:post, %r{\A#{Settings.credentials.apply.host}/oauth/token\z}).to_return(status: 200, body: faked_response)
+    stub_request(:post, %r{\A#{Settings.credentials.apply.host}/oauth/token\z}).to_return(status: 200, body: fake_data)
   end
 
   describe '.call' do
