dc9599349c5453d8300e7140eb152ceb30a485a3
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-06-06 21:46:41 +0100

    Update redis config
    Create a MockRedis instance if in test

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,6 @@
-REDIS = Redis::Namespace.new('lhisa_uat', redis: Redis.new)
+REDIS = case Rails.env
+        when 'test'
+          MockRedis.new
+        else
+          Redis::Namespace.new('lhisa_uat', redis: Redis.new)
+        end
