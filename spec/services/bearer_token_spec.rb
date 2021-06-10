867639db49f6ccfe76207dbf080425659218cdbe
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-06-10 17:47:09 +0100

    Add bearer_token class
    Implement the creation of token for the provided use case
    along with stubbed tests

---
+++ spec/services/bearer_token_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe BearerToken do
+  subject(:bearer_token) { described_class.new('use_case_one') }
+
+  let(:fake_data) do
+    {
+      access_token: 'zz00000z00z0z00000z0z0z0000z0000',
+      token_type: 'bearer'
+      # real response has other returns but out of scope of this test
+    }.to_json
+  end
+
+  before do
+    stub_request(:post, %r{\A#{Settings.credentials.use_case_one.host}/oauth/token\z})
+      .to_return(status: 200, body: fake_data)
+  end
+
+  context 'when called with an invalid use_case' do
+    subject(:bearer_token) { described_class.new('not_a_use_case') }
+
+    it { expect { subject }.to raise_error 'Unsupported UseCase' }
+  end
+
+  describe '#call' do
+    subject(:call) { bearer_token.call }
+
+    it { is_expected.to eql 'zz00000z00z0z00000z0z0z0000z0000' }
+  end
+
+  describe '.call' do
+    subject(:call) { described_class.call('use_case_one') }
+    it { is_expected.to eql 'zz00000z00z0z00000z0z0z0000z0000' }
+  end
+end
