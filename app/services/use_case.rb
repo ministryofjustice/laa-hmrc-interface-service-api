5fb27b493fb781a1d204f0a0050bed9e78fb40a1
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-06-10 17:46:47 +0100

    Implement a UseCase class
    This will be expanded for future use but for now
    allows the call of a sync test for benchmarking

---
+++ app/services/use_case.rb
@@ -0,0 +1,33 @@
+class UseCase
+  FOUR_HOURS = 14_400 # fours hours in seconds
+
+  def initialize(use_case)
+    @use_case = "use_case_#{use_case}"
+    @bearer_token = bearer_token
+  end
+
+  # :nocov:
+  # This is ignored for coverage as it allows a legacy script to run
+  # It should be removed once a new set of sidekiq jobs is created
+  # that will allow a comparison before this is deleted
+  def run_sync_test(first_name, last_name, date_of_birth, nino)
+    Benchmark.measure { SyncTest.new(first_name, last_name, date_of_birth, nino).call }.real
+  end
+  # :nocov:
+
+  private
+
+  def bearer_token
+    generate_new_token if current_token.nil?
+    current_token
+  end
+
+  def generate_new_token
+    new_token = BearerToken.call(@use_case)
+    REDIS.setex("#{@use_case}_bearer_token", FOUR_HOURS, new_token)
+  end
+
+  def current_token
+    REDIS.get("#{@use_case}_bearer_token")
+  end
+end
