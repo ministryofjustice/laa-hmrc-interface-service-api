cb669421b4c6196baabb3e8154913b0d43b7ccc0
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-06-10 14:17:42 +0100

    Update status controller
    Add redis to the health_check output

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'main#show'
   get 'main', to: 'main#show', format: :json
   get 'ping', to: 'status#ping', format: :json
-  get 'healthcheck', to: 'status#status', format: :json
+  get 'health_check', to: 'status#health_check', format: :json
   get 'status', to: 'status#ping', format: :json
 end
