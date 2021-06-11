cd7e7a2af731d98a936dd621a4d84d8cd6a7444e
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-06-11 08:02:39 +0100

    Update application_user factory
    Use the same generator as we would in use

--- spec/factories/application_user.rb
+++ spec/factories/application_user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :application_user do
     name { Faker::Company.name }
     access_key { SecureRandom.hex(13) }
-    secret_key { Faker::Blockchain::Tezos.secret_key }
+    secret_key { SecureRandom.hex(64) }
     use_cases { [1] }
   end
 end
