afc11179ed57b10ebad82b6a53040994344ef09d
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-06-18 14:38:32 +0100

    WIP: Update CallEndPointWorker
    Tests are a bit janky and mainly for coverage

--- app/workers/call_endpoint_worker.rb
+++ app/workers/call_endpoint_worker.rb
@@ -1,11 +1,13 @@
 class CallEndpointWorker < TaskWorker
   include Sidekiq::Worker
 
-  def perform(task, href)
-    @task = task
+  def perform(task_id, href)
+    @task = Task.find(task_id)
     @href = href
+    @task.update(calls_started: @task.calls_started + 1)
     response = RestClient.get(url, headers)
     parsed_data = JSON.parse(response)
+    Rails.logger.info "write #{parsed_data} to task.outcome"
     #   write_to_file(JSON.pretty_generate(data))
     #   return data
     # rescue RestClient::TooManyRequests
@@ -17,7 +19,7 @@ class CallEndpointWorker < TaskWorker
   private
 
   def url
-    "#{use_case.host}/individuals/matching"
+    "#{use_case.host}#{parse_url_params}"
   end
 
   def headers
@@ -37,12 +39,12 @@ class CallEndpointWorker < TaskWorker
     end
   end
 
-  def parse_params
+  def parse_url_params
     case @href
     when /{&fromDate,toDate}/
-      @href.gsub!('{&fromDate,toDate}', "&fromDate=#{data[:from]}&toDate=#{data[:to]}")
+      @href.gsub!('{&fromDate,toDate}', "&fromDate=#{start_date}&toDate=#{end_date}")
     when /{&fromTaxYear,toTaxYear}/
-      @href.gsub!('{&fromTaxYear,toTaxYear}', "&fromTaxYear=#{data[:from]}&toTaxYear=#{data[:to]}&useCase=#{use_case_name}")
+      @href.gsub!('{&fromTaxYear,toTaxYear}', "&fromTaxYear=#{start_year}&toTaxYear=#{end_year}&useCase=#{use_case_name}")
     else
       @href
     end
@@ -55,5 +57,21 @@ class CallEndpointWorker < TaskWorker
       three: 'LAA-C3',
       four: 'LAA-C4'
     }[@task.use_case]
+  end
+
+  def start_date
+    @start_date ||= data[:from]
+  end
+
+  def end_date
+    @end_date ||= data[:to]
+  end
+
+  def start_year
+    "#{Date.parse(start_date).year - 1}-#{Date.parse(start_date).strftime('%y')}"
+  end
+
+  def end_year
+    "#{Date.parse(end_date).year - 1}-#{Date.parse(end_date).strftime('%y')}"
   end
 end
--- spec/workers/call_endpoint_worker_spec.rb
+++ spec/workers/call_endpoint_worker_spec.rb
@@ -3,5 +3,48 @@ require 'rails_helper'
 RSpec.describe CallEndpointWorker do
   subject(:worker) { described_class.new }
   let(:task) { create :task }
+  before do
+    allow_any_instance_of(BearerToken).to receive(:call).and_return('new_fake_token_value')
+    stub_request(:get, %r{\A#{Settings.credentials.host}/fake/.*\z}).to_return(status: 200, body: result)
+  end
+  let(:result) { valid_result }
+  let(:valid_result) do
+    '{"_links":{"individual":{"name":"GET","href":"/individuals/matching/8c4a59f1-e7c4-471e-95e7-f6cdc54286d1",' \
+    '"title":"Get a matched individualxE2x80x99s information"},"self":{"href":"/individuals/matching/"}}}'
+  end
+
   it { is_expected.to be_a CallEndpointWorker }
+
+  describe '.perform' do
+    subject(:perform) { worker.perform(task.id, test_url) }
+    let(:test_url) { '/fake/url' }
+
+    it 'updates the task.calls_completed' do
+      expect { perform }.to change { task.reload.calls_started }.by(1)
+    end
+
+    context 'when the url contains benefits_and_credits' do
+      let(:test_url) { '/fake/benefits-and-credits/url' }
+
+      it 'parses the correct headers and still succeeds' do
+        expect { perform }.to change { task.reload.calls_started }.by(1)
+      end
+    end
+
+    context 'when the url contains {&fromDate,toDate}' do
+      let(:test_url) { '/fake/benefits-and-credits/url{&fromDate,toDate}' }
+
+      it 'parses the correct headers and still succeeds' do
+        expect { perform }.to change { task.reload.calls_started }.by(1)
+      end
+    end
+
+    context 'when the url contains {&fromTaxYear,toTaxYear}' do
+      let(:test_url) { '/fake/benefits-and-credits/url{&fromTaxYear,toTaxYear}' }
+
+      it 'parses the correct headers and still succeeds' do
+        expect { perform }.to change { task.reload.calls_started }.by(1)
+      end
+    end
+  end
 end
