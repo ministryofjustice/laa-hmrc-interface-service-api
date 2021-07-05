63b52ccd45f1411a067bbe6c78ee719a59a2b2da
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-07-05 11:36:31 +0100

    Update sentry to ignore badly named error

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,9 @@ Sentry.init do |config|
   config.dsn = Settings.sentry.dsn unless Settings.sentry.environment.eql?('test')
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.environment = Settings.sentry.environment
-
+  config.excluded_exceptions += %w[
+    TestWorker::SentryIgnoresThisSidekiqFailError
+  ]
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production
