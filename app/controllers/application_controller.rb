1419776fb6c687aab05814c3a9ba6699fed2512f
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2021-08-08 12:27:29 +0100

    Set Doorkeeper to authorize by default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :doorkeeper_authorize!
+
+  ERROR_MAPPINGS = {
+    ActionController::ParameterMissing => :bad_request,
+    Errors::ContractError => :unprocessable_entity
+  }.freeze
+
+  # :nocov:
+  # nocov added because this isn't implemented
+  # in a controller yet, but it should be!
+  ERROR_MAPPINGS.each do |klass, status|
+    rescue_from klass do |error|
+      render json: { error: error }, status: status
+    end
+  end
+  # :nocov:
 end
