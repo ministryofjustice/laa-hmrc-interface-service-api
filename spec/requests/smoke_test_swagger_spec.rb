a370d4cff835fd02dbf0780d38d4e0f342fc174b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-08-09 07:06:12 +0100

    Update SmokeTestSwagger
    Now the smoke test endpoint has changed it needs a different stub

--- spec/requests/smoke_test_swagger_spec.rb
+++ spec/requests/smoke_test_swagger_spec.rb
@@ -3,11 +3,15 @@ require 'swagger_helper'
 RSpec.describe 'smoke_test', type: :request, swagger_doc: 'v1/swagger.yaml' do
   path '/smoke-test' do
     before do
-      allow_any_instance_of(SmokeTest).to receive(:call).and_return(true)
+      allow(REDIS).to receive(:get).with('smoke-test-one').and_return(true)
+      allow(REDIS).to receive(:get).with('smoke-test-two').and_return(true)
+      allow(REDIS).to receive(:get).with('smoke-test-three').and_return(true)
+      allow(REDIS).to receive(:get).with('smoke-test-four').and_return(true)
     end
 
     get('call smoke_test') do
-      response(200, 'true') do
+      description 'Fetch recent smoke_test results'
+      response(200, 'Success') do
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
