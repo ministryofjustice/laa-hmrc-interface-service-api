22722b1bb7b78cea71505377cdf8ac3be2f0be78
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-08-31 09:37:24 +0100

    AP-2459: Update submission model with ActiveStorage
    Allow storing of a result document

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,10 @@
 class Submission < ApplicationRecord
+  has_one_attached :result
+
   USE_CASES = %w[one two three four].freeze
   validates :use_case, presence: true, inclusion: { in: USE_CASES, message: 'Invalid use case' }
+
+  def as_json(*)
+    super.except('id', 'status', 'created_at', 'updated_at')
+  end
 end
