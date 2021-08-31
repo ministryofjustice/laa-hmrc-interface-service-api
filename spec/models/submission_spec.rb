5ee3646a7f58044f3b6afb61cdb5a4f6215c227a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-08-31 12:10:34 +0100

    AP-2459: Add as_json tests to submission
    Ensure the expected keys _are_ returned as well as
    excluding the removed values

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -3,9 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe Submission, type: :model do
-  subject { described_class.new }
+  subject(:submission) { described_class.new(params) }
+
+  let(:params) do
+    {
+      use_case: 'one',
+      first_name: 'Bob',
+      last_name: 'Smith',
+      nino: 'XX123456Y',
+      dob: '1990-01-01',
+      start_date: '2021-01-01',
+      end_date: '2021-03-31'
+    }
+  end
 
   it {
     is_expected.to validate_inclusion_of(:use_case).in_array(%w[one two three four]).with_message(/Invalid use case/)
   }
+
+  describe '.as_json' do
+    subject(:as_json) { submission.as_json }
+
+    it 'returns the expected values' do
+      expect(as_json.keys).to match_array %w[use_case first_name last_name nino dob start_date end_date]
+    end
+
+    it 'does not include standard model values' do
+      expect(as_json.keys).to_not include %w[id status created_at updated_at]
+    end
+  end
 end
