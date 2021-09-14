1fbfaf8ad61c2a1583bd486d2eab28f38338d08b
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2021-09-14 13:32:10 +0100

    Refactor solution for ap-2482 to use a service - submission_service.rb
    and remove .process! code from model

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe Submission, type: :model, vcr: { cassette_name: 'use_case_one_success' } do
+RSpec.describe Submission, type: :model do
   subject(:submission) { described_class.create(params) }
 
   let(:params) do
@@ -30,47 +30,6 @@ RSpec.describe Submission, type: :model, vcr: { cassette_name: 'use_case_one_suc
 
     it 'does not include standard model values' do
       expect(as_json.keys).to_not include %w[id status created_at updated_at]
-    end
-  end
-
-  describe '#process!' do
-    subject(:process!) { submission.process! }
-
-    before do
-      remove_request_stub(@hmrc_stub_requests)
-      allow(REDIS).to receive(:get).with('use_case_one_bearer_token').and_return('dummy_bearer_token')
-    end
-
-    it 'adds a result attachment to the submission' do
-      expect do
-        subject
-      end.to change(ActiveStorage::Attachment, :count).by(1)
-    end
-
-    it 'gives the result attachment the a filename' do
-      subject
-      expect(submission.result.filename).to eq "#{submission.id}.json"
-    end
-
-    it 'gives the result attachment a key' do
-      subject
-      expect(submission.result.key).to eq "submission/result/#{submission.id}"
-    end
-
-    it 'gives the result attachment a content type' do
-      subject
-      expect(submission.result.content_type).to eq 'application/json'
-    end
-
-    context 'RestClient::InternalServerError' do
-      before do
-        allow(RestClient).to receive(:get).with(any_args).and_raise(RestClient::InternalServerError)
-      end
-
-      it 'adds a result attachment detailing the error' do
-        subject
-        expect(submission.result.blob.download).to match(/returned INTERNAL_SERVER_ERROR/)
-      end
     end
   end
 end
