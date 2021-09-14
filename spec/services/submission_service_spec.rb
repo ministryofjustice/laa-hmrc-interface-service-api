24dc0a7c5e21327006478d91fbe7d9bdde3d11d8
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2021-09-14 13:32:10 +0100

    Refactor solution for ap-2482 to use a service - submission_service.rb
    and remove .process! code from model

---
+++ spec/services/submission_service_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe SubmissionService, vcr: { cassette_name: 'use_case_one_success' } do
+  describe '.call' do
+    subject(:call) { described_class.call(submission) }
+    let(:submission) { create :submission, data }
+    let(:data) do
+      {
+        use_case: 'one',
+        first_name: 'Langley',
+        last_name: 'Yorke',
+        nino: 'MN212451D',
+        dob: '1992-07-22',
+        start_date: '2020-08-01',
+        end_date: '2020-10-01'
+      }
+    end
+
+    before do
+      remove_request_stub(@hmrc_stub_requests)
+      allow(REDIS).to receive(:get).with('use_case_one_bearer_token').and_return('dummy_bearer_token')
+    end
+
+    it 'adds a result attachment to the submission' do
+      expect do
+        subject
+      end.to change(ActiveStorage::Attachment, :count).by(1)
+    end
+
+    it 'gives the submission result attachment the a filename' do
+      subject
+      expect(submission.result.filename).to eq "#{submission.id}.json"
+    end
+
+    it 'gives the submission result attachment a key' do
+      subject
+      expect(submission.result.key).to eq "submission/result/#{submission.id}"
+    end
+
+    it 'gives the submission result attachment a content type' do
+      subject
+      expect(submission.result.content_type).to eq 'application/json'
+    end
+
+    context 'RestClient::InternalServerError' do
+      before do
+        allow(RestClient).to receive(:get).with(any_args).and_raise(RestClient::InternalServerError)
+      end
+
+      it 'adds a result attachment detailing the error' do
+        subject
+        expect(submission.result.blob.download).to match(/returned INTERNAL_SERVER_ERROR/)
+      end
+    end
+  end
+end
