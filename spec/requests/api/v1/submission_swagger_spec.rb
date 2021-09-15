c591ec21ef7f2eddc875977ea520cb6bd89a171b
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2021-09-15 17:56:34 +0100

    AP-2493 Add GET submission endpoint
    Adds a new route and controller to return the contents of a submission's
    result object. Documents the new endpoint with swagger.
    
    * Returns HTTP200 + the result if the submission has completed and the
    result object exists.
    
    * Returns HTTP500 if the submission has completed but no result object
    exists.
    
    * Returns HTTP202 if the submission has not yet completed.
    
    * Returns HTTP404 if the submission does not exist.
    
    * Returns HTTP401 if the request is not authenticated.

---
+++ spec/requests/api/v1/submission_swagger_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+require 'swagger_helper'
+
+RSpec.describe 'GET submission', type: :request, swagger_doc: 'v1/swagger.yaml' do
+  include AuthorisedRequestHelper
+
+  let(:token) { application.access_tokens.create! }
+  let(:Authorization) { "Bearer #{token.token}" }
+  let(:application) { dk_application }
+  let(:id) { submission.id }
+
+  path '/api/v1/submission/{id}' do
+    get 'Retrieves a submission' do
+      tags 'submission'
+      produces 'application/json'
+      consumes 'application/json'
+      security [{ oAuth: [] }]
+      parameter name: :id, in: :path, type: :string
+
+      response 200, 'completed submission found' do
+        let!(:submission) { create :submission, :completed, :with_attachment }
+        let(:expected_response) { { data: 'test_data' } }
+        run_test! do |response|
+          expect(response.media_type).to eq('application/json')
+          expect(JSON.parse(response.body).symbolize_keys).to eq(expected_response)
+        end
+      end
+
+      response 202, 'incomplete submission found' do
+        let!(:submission) { create :submission, :in_progress }
+        run_test!
+      end
+
+      response 500, 'submission has been completed but no result object is present' do
+        let!(:submission) { create :submission, :completed }
+        run_test!
+      end
+
+      response 404, 'submission not found' do
+        let(:id) { '1234' }
+        run_test!
+      end
+
+      response 401, 'Error: Unauthorized' do
+        let(:id) { '1234' }
+        let(:Authorization) { nil }
+        run_test!
+      end
+    end
+  end
+end
