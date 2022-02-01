162d0ed9d236dd42d2e0a23a4b0f5097b77d94c4
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-02-01 09:37:38 +0000

    Address Layout/SpaceInsideArrayLiteralBrackets and Layout/SpaceInsidePercentLiteralDelimiters
    using autocorrect

--- db/migrate/20210916154020_change_active_storage_ids_to_uuid.rb
+++ db/migrate/20210916154020_change_active_storage_ids_to_uuid.rb
@@ -15,7 +15,7 @@ class ChangeActiveStorageIdsToUuid < ActiveRecord::Migration[6.1]
       t.belongs_to :blob, null: false, index: false
       t.string :variation_digest, null: false
 
-      t.index %i[ blob_id variation_digest ], name: "index_active_storage_variant_records_uniqueness", unique: true
+      t.index %i[blob_id variation_digest], name: "index_active_storage_variant_records_uniqueness", unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
 
@@ -29,7 +29,7 @@ class ChangeActiveStorageIdsToUuid < ActiveRecord::Migration[6.1]
       t.string   :checksum,     null: false
       t.datetime :created_at,   null: false
 
-      t.index [ :key ], unique: true
+      t.index [:key], unique: true
     end
 
     create_table :active_storage_blobs, id: :uuid do |t|
@@ -42,7 +42,7 @@ class ChangeActiveStorageIdsToUuid < ActiveRecord::Migration[6.1]
       t.string   :checksum,     null: false
       t.datetime :created_at,   null: false
 
-      t.index [ :key ], unique: true
+      t.index [:key], unique: true
     end
 
     create_table :active_storage_attachments, id: :uuid do |t|
@@ -60,7 +60,7 @@ class ChangeActiveStorageIdsToUuid < ActiveRecord::Migration[6.1]
       t.belongs_to :blob, null: false, index: false, type: :uuid
       t.string :variation_digest, null: false
 
-      t.index %i[ blob_id variation_digest ], name: "index_active_storage_variant_records_uniqueness", unique: true
+      t.index %i[blob_id variation_digest], name: "index_active_storage_variant_records_uniqueness", unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id, type: :uuid
     end
   end
