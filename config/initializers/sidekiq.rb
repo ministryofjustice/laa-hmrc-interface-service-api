50a26f6e7b6605c647339e8c16a9fa0452636bb6
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-01-25 16:29:02 +0000

    Add Sidekiq.configure_client options
    Version 7.x outputs INFO to test suite. This
    silences it by ony outputting Warnings+.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,8 @@
-Sidekiq.configure_server do |cfg|
-  cfg.logger = Rails.logger
+Sidekiq.configure_server do |config|
+  config.logger = Rails.logger
+end
+
+Sidekiq.configure_client do |config|
+  config.logger = Rails.logger
+  config.logger.level = Logger::WARN # prevent info output in tests
 end
