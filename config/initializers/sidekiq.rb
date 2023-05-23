ebbfdeb0a0275a1fd248ce8a55ad9ccf87dc09bf
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-05-23 15:06:08 +0100

    Add a bin/dev command for running locally
    This runs the app in a manner that emulates how
    it runs in production (with 5 unique queues with matching
    concurrency). It further isolates its local redis database
    and web port from other local redis databases and web ports
    so you can run a consumer app locally and still monitor the
    queues through the app specific sidekiq web UIs.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,10 @@
 Sidekiq.configure_server do |config|
   config.logger = Rails.logger
+  config.redis = { url: 'redis://127.0.0.1:6379/5' } if Rails.env.development?
 end
 
 Sidekiq.configure_client do |config|
   config.logger = Rails.logger
   config.logger.level = Logger::WARN # prevent info output in tests
+  config.redis = { url: 'redis://127.0.0.1:6379/5' } if Rails.env.development?
 end
