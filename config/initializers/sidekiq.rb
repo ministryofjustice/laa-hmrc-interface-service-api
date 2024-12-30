b738c6baf514b25c06c5739510146982b4a57d3a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-12-30 15:33:44 +0000

    Create capsules and remove standalone queues

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,10 @@
 Sidekiq.configure_server do |config|
   config.logger = Rails.logger
   config.redis = { url: "redis://127.0.0.1:6380/5" } if Rails.env.development?
+  config.capsule("submission_capsules") do |cap|
+    cap.concurrency = 1
+    cap.queues = %w[uc_one_submissions uc_two_submissions uc_three_submissions uc_one_submissions]
+  end
 end
 
 Sidekiq.configure_client do |config|
